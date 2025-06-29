f601b9f3c0522bab6716ffa4c594a69576d7cf24
Periicles <landescrew.scoot@gmail.com>
2025-06-29 22:29:43 +0200

    refactor: GET route better

--- src/app/api/locations/[id]/route.ts
+++ src/app/api/locations/[id]/route.ts
@@ -1,22 +1,29 @@
 import { prisma } from '@/lib/prisma';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id;
 
   const location = await prisma.location.findUnique({
     where: { location_id: id },
   });
 
-  if (!location) return NextResponse.json({ error: 'Lieu non trouvé' }, { status: 404 });
+  if (!location) {
+    return NextResponse.json({ error: 'Lieu non trouvé' }, { status: 404 });
+  }
 
   return NextResponse.json(location);
 }
 
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+export async function PUT(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   const data = await req.json();
-  const { id } = context.params;
+  const id = context.params.id;
 
   const updated = await prisma.location.update({
     where: { location_id: id },
@@ -34,8 +41,11 @@ export async function PUT(req: NextRequest, context: { params: { id: string } })
   return NextResponse.json(updated);
 }
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function DELETE(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id;
 
   await prisma.location.delete({
     where: { location_id: id },
